089024dbeccc9f23225b811fcc8cc9f19042afad
Awais iftikhar <awaisiftikhar90@gmail.com>
2023-10-05 15:24:23 -0500

    elibrary project SB

---
+++ CS489-ApplSoftwareDev/Lab3/elibrary/src/test/java/edu/mum/cs/cs489/Lab3eLibrary/ElibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mum.cs.cs489.Lab3eLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
