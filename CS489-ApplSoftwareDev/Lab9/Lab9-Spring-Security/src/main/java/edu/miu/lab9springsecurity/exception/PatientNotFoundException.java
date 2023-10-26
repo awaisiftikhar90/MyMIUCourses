4a715551ac19b2284cb1a5e1422ecc3afa5db13f
Awais iftikhar <awaisiftikhar90@gmail.com>
2023-10-26 10:24:00 -0500

    Security

--- CS489-ApplSoftwareDev/Lab9/Lab9-Spring-Security/src/main/java/edu/miu/lab9springsecurity/exception/PatientNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package edu.miu.lab9springsecurity.exception;
-
-public class PatientNotFoundException extends Exception{
-    public PatientNotFoundException(String message) {
-        super(message);
-    }
-}
--- CS489-ApplSoftwareDev/Lab9/Lab9-Spring-Security/src/main/java/edu/miu/lab9springsecurity/repository/AddressRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.miu.lab9springsecurity.repository;
-
-import edu.miu.lab9springsecurity.models.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Integer> {
-
-}
