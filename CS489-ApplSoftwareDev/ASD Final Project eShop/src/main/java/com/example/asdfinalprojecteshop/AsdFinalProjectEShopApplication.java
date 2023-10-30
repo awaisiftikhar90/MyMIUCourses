d8f54e626e4d825fae374fb3a86dfd5bd3f4be62
Awais iftikhar <awaisiftikhar90@gmail.com>
2023-10-30 12:58:33 -0500

    ASDProject ART

---
+++ CS489-ApplSoftwareDev/ASD Final Project eShop/src/main/java/com/example/asdfinalprojecteshop/AsdFinalProjectEShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.asdfinalprojecteshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsdFinalProjectEShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsdFinalProjectEShopApplication.class, args);
+    }
+
+}
