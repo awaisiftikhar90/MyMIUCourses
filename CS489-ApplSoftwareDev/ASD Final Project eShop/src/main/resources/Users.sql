949b63208865b54f5c4110aefa973d9b1fd88c1b
Awais Iftikhar <awaisiftikhar90@gmail.com>
2023-11-04 02:39:18 -0500

    SQLDB

---
+++ CS489-ApplSoftwareDev/ASD Final Project eShop/src/main/resources/Users.sql
@@ -0,0 +1,5 @@
+/*
+-- Query: SELECT * FROM eShop.users
+*/
+INSERT INTO users (`customer_id`,`email`,`password`,`phone_number`,`username`)
+VALUES (2,'awais@miu.edu','awais','3239425222','Awais Iftikhar');
