5e063233395c99bde4b894dbd21d9bd81e27018b
Awais Iftikhar <awaisiftikhar90@gmail.com>
2023-11-04 03:18:41 -0500

    code updated

--- CS489-ApplSoftwareDev/ASD Final Project eShop/src/main/java/com/example/asdfinalprojecteshop/controller/CategoryController.java
+++ CS489-ApplSoftwareDev/ASD Final Project eShop/src/main/java/com/example/asdfinalprojecteshop/controller/CategoryController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-//@CrossOrigin(origins = "http://localhost:3000")
+
 @RequestMapping(value = {"api", "api/categories"})
 public class CategoryController {
     private final CategoryService categoryService;
